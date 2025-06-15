72d51da02b9986412ca3701f6c89330acb72d9c6
dinesh-veer <dveer123@hotmail.com>
2025-06-15 12:53:07 +0530

    Updated code with H2 DB

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,11 +1,9 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.example.demo.model.Employee;
-import io.micrometer.observation.ObservationFilter;
 
 public interface EmployeeService {
 
