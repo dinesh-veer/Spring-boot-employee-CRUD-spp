1420c710d7364d14cce2ef1df6bc5e024ae71e0f
dinesh-veer <dveer123@hotmail.com>
2025-06-18 00:16:10 +0530

    Updated default loading of DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- src/main/resources/data.sql
+INSERT INTO PRODUCTS (name, price) VALUES ('Laptop', 1200.00);
+INSERT INTO PRODUCTS (name, price) VALUES ('Mouse', 25.50);
+INSERT INTO PRODUCTS (name, price) VALUES ('Keyboard', 75.00);
+
+INSERT INTO EMPLOYEE(name,department,email,salary) values ('DInesh','CS','dinesh@gmail.com',40);
+INSERT INTO EMPLOYEE(name,department,email,salary) values ('Satish','MECH','satish@gmail.com',67);
+INSERT INTO EMPLOYEE(name,department,email,salary) values ('Ganesh','CHEM','ganesh@gmail.com',53);
+INSERT INTO EMPLOYEE(name,department,email,salary) values ('Rajesh','IT','raj@gmail.com',90);
